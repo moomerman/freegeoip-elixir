265269889f270e09e5864b2b0e486dc89f6af982
Julio Jiménez <jjimenezm@gmail.com>
2016-03-01 18:01:59 +0100

    Add support for basic auth on custom geoip server and reading from system env vars

--- lib/freegeoip.ex
+++ lib/freegeoip.ex
@@ -47,7 +47,13 @@ defmodule FreeGeoIP do
   Returns binary
   """
   def config_base_url do
-    Application.get_env(:freegeoip, :base_url) || @default_base_url
+    Application.get_env(:freegeoip, :base_url) || System.get_env("FREEGEOIP_HOST") || @default_base_url
+  end
+  def config_auth_user do
+    Application.get_env(:freegeoip, :auth_user) || System.get_env("FREEGEOIP_USER")
+  end
+  def config_auth_password do
+    Application.get_env(:freegeoip, :auth_password) || System.get_env("FREEGEOIP_PASSWORD")
   end
 
   @doc """
@@ -62,16 +68,47 @@ defmodule FreeGeoIP do
 
 
   """
-  def make_request( method, endpoint, body \\ [], headers \\ [], options \\ []) do
+  def make_request( method, endpoint, body \\ []) do
     url = config_base_url <> endpoint
-    case request(method, url, body, headers, options) do
+
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Accept", "application/json"}
+    ]
+
+    headers = case auth_header(config_auth_user, config_auth_password) do
+      nil -> headers
+      auth -> [auth | headers]
+    end
+
+    case request(method, url, body, headers) do
       {:ok, response} ->
-        case response.body do
-          "Try again later\n" -> {:error, %{reason: :process_timeout, error: "freegeoip server seems to be oversaturated."}}
-          body -> body
+        case response.status_code do
+          401 ->
+            {:error, %{reason: :auth_failed, error: "freegeoip server requires basic auth credentials"}}
+          200 ->
+            case response.body do
+              "Try again later\n" ->
+                {:error, %{reason: :process_timeout, error: "freegeoip server seems to be oversaturated"}}
+              body -> body
+            end
+          state ->
+            {:error, %{reason: :invalid_state, error: "freegeoip request returned an invalid HTTP state: " <> state}}
         end
+
       {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, %{reason: reason, error: "Error connecting to freegeoip server."}}
+        {:error, %{reason: reason, error: "error connecting to freegeoip server"}}
+    end
+  end
+
+  #
+  # Private
+  #
+
+  defp auth_header(username, password) do
+    if (username && password) do
+      encoded = Base.encode64("#{username}:#{password}")
+      {"Authorization", "Basic #{encoded}"}
     end
   end
 
